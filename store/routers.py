ecc6b918df8d3b933b720554d72185f0ec08215e
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2025-08-28 01:13:11 -0300

    uptade

--- store/routers.py
+++ store/routers.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
 from store.controllers.product import router as product
+from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(product, prefix="/products")
